049707c62e063da266739a16aed6784fd1b6e200
Viliam Durina <viliam@hazelcast.com>
2017-04-25 09:46:40 +0200

    Add Flink latency benchmark

---
+++ trade-monitor/flink-trade-monitor/src/main/java/com/hazelcast/jet/benchmark/trademonitor/FlinkLatencyMonitor.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2008-2017, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.jet.benchmark.trademonitor;
+
+import org.apache.flink.api.common.JobExecutionResult;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.core.fs.FileSystem.WriteMode;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks;
+import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
+import org.apache.flink.streaming.util.serialization.AbstractDeserializationSchema;
+import org.apache.flink.streaming.util.serialization.DeserializationSchema;
+import org.apache.flink.util.Collector;
+import org.apache.kafka.common.serialization.LongDeserializer;
+
+import java.io.IOException;
+import java.util.Properties;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class FlinkLatencyMonitor {
+
+    private static final AtomicLong totalSum = new AtomicLong();
+    private static final AtomicLong totalCount = new AtomicLong();
+
+    public static void main(String[] args) throws Exception {
+        if (args.length < 3 || args.length > 4) {
+            System.err.println("Usage:");
+            System.err.println("  "+FlinkLatencyMonitor.class.getSimpleName()+" <bootstrap.servers> <topic> <initialOffset:earliest/latest> [<outputFile>]");
+            System.exit(1);
+        }
+        System.setProperty("hazelcast.logging.type", "log4j");
+        String brokerUri = args[0];
+        String topic = args[1];
+        String initialOffset = args[2];
+        String fileName = args.length > 3 ? args[3] : null;
+
+        // set up the execution environment
+        StreamExecutionEnvironment env =
+                StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
+        DeserializationSchema<Trade> schema = new AbstractDeserializationSchema<Trade>() {
+            TradeDeserializer deserializer = new TradeDeserializer();
+
+            @Override
+            public Trade deserialize(byte[] message) throws IOException {
+                return deserializer.deserialize(null, message);
+            }
+        };
+
+        DataStreamSource<Trade> trades = env.addSource(new FlinkKafkaConsumer010<>(topic,
+                schema, getKafkaProperties(brokerUri, initialOffset)));
+        AssignerWithPeriodicWatermarks<Trade> timestampExtractor
+                = new BoundedOutOfOrdernessTimestampExtractor<Trade>(Time.milliseconds(1000)) {
+            @Override
+            public long extractTimestamp(Trade element) {
+                return element.getTime();
+            }
+        };
+
+        SingleOutputStreamOperator<Tuple3<Long, String, Long>> finalStream = trades
+                .map((Trade trade) -> {
+                    trade.setIngestionTime(System.nanoTime());
+                    return trade;
+                })
+                .assignTimestampsAndWatermarks(timestampExtractor)
+                .keyBy(Trade::getTicker)
+                .window(SlidingEventTimeWindows.of(Time.milliseconds(10000), Time.milliseconds(1000)))
+                .reduce(new ReduceFunction<Trade>() {
+                    @Override
+                    public Trade reduce(Trade value1, Trade value2) throws Exception {
+                        return value1.getIngestionTime() > value2.getIngestionTime() ? value1 : value2;
+                    }
+                }, new WindowFunction<Trade, Tuple3<Long, String, Long>, String, TimeWindow>() {
+                    @Override
+                    public void apply(String key, TimeWindow window,
+                            Iterable<Trade> input,
+                            Collector<Tuple3<Long, String, Long>> out) throws Exception {
+                        Trade latestTrade = input.iterator().next();
+                        out.collect(new Tuple3<>(window.getEnd(), key, latestTrade.getIngestionTime()));
+                    }
+                });
+        finalStream
+                .addSink(new RichSinkFunction<Tuple3<Long, String, Long>>() {
+                    @Override
+                    public void invoke(Tuple3<Long, String, Long> value) throws Exception {
+                        long latency = System.nanoTime() - value.f2;
+                        //logger.info("sink1-frame drained=" + drainedCount + " frames, now=" + System.nanoTime() + ", iterationCnt=" + iterationCnt);
+
+                        totalSum.addAndGet(latency);
+                        totalCount.addAndGet(1);
+                    }
+                });
+        if (fileName != null) {
+            finalStream
+                    .writeAsCsv(fileName, WriteMode.OVERWRITE);
+        }
+
+        new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    return;
+                }
+                long sum = totalSum.get();
+                long count = totalCount.get();
+                totalSum.set(0);
+                totalCount.set(0);
+                System.out.println("average latency=" + (count != 0 ? sum / count / 1_000_000 + "ms" : "?") + ", count=" + count);
+            }
+        }).start();
+
+        JobExecutionResult execute = env.execute("Trade Monitor Example");
+    }
+
+    private static Properties getKafkaProperties(String brokerUrl, String initialOffset) {
+        Properties props = new Properties();
+        props.setProperty("bootstrap.servers", brokerUrl);
+        props.setProperty("group.id", UUID.randomUUID().toString());
+        props.setProperty("key.deserializer", LongDeserializer.class.getName());
+        props.setProperty("value.deserializer", TradeDeserializer.class.getName());
+        props.setProperty("auto.offset.reset", initialOffset);
+        props.setProperty("max.poll.records", "32768");
+        return props;
+    }
+}
