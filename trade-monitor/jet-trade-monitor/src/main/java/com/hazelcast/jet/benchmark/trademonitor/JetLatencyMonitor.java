0f99880c3500a6ab463cd85a5fb1b825f9d1bba5
Viliam Durina <viliam@hazelcast.com>
2017-04-26 09:25:38 +0200

    Two-step aggregation of latencies, output to file for Jet

--- trade-monitor/jet-trade-monitor/src/main/java/com/hazelcast/jet/benchmark/trademonitor/JetLatencyMonitor.java
+++ trade-monitor/jet-trade-monitor/src/main/java/com/hazelcast/jet/benchmark/trademonitor/JetLatencyMonitor.java
@@ -3,24 +3,18 @@ package com.hazelcast.jet.benchmark.trademonitor;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.jet.AbstractProcessor;
 import com.hazelcast.jet.DAG;
-import com.hazelcast.jet.Inbox;
+import com.hazelcast.jet.Distributed;
+import com.hazelcast.jet.Distributed.Function;
 import com.hazelcast.jet.Jet;
 import com.hazelcast.jet.JetInstance;
-import com.hazelcast.jet.Outbox;
-import com.hazelcast.jet.Processor;
-import com.hazelcast.jet.Punctuation;
 import com.hazelcast.jet.Vertex;
 import com.hazelcast.jet.windowing.Frame;
 import com.hazelcast.jet.windowing.WindowDefinition;
 import com.hazelcast.jet.windowing.WindowOperation;
-import com.hazelcast.jet.windowing.WindowingProcessors;
-import com.hazelcast.logging.ILogger;
 import org.apache.kafka.common.serialization.LongDeserializer;
 
-import java.util.Map;
 import java.util.Properties;
 import java.util.UUID;
-import java.util.concurrent.atomic.AtomicLong;
 
 import static com.hazelcast.jet.DistributedFunctions.entryValue;
 import static com.hazelcast.jet.Edge.between;
@@ -30,34 +24,88 @@ import static com.hazelcast.jet.Processors.map;
 import static com.hazelcast.jet.Processors.writeFile;
 import static com.hazelcast.jet.connector.kafka.StreamKafkaP.streamKafka;
 import static com.hazelcast.jet.windowing.PunctuationPolicies.cappingEventSeqLag;
+import static com.hazelcast.jet.windowing.WindowingProcessors.groupByFrame;
 import static com.hazelcast.jet.windowing.WindowingProcessors.insertPunctuation;
 import static com.hazelcast.jet.windowing.WindowingProcessors.slidingWindow;
 
 public class JetLatencyMonitor {
 
-    private static final AtomicLong totalSum = new AtomicLong();
-    private static final AtomicLong totalCount = new AtomicLong();
-
     public static void main(String[] args) throws Exception {
-        if (args.length < 3 || args.length > 4) {
+        if (args.length != 4) {
             System.err.println("Usage:");
-            System.err.println("  "+JetLatencyMonitor.class.getSimpleName()+" <bootstrap.servers> <topic> <slideByMs> [<outputFile>]");
+            System.err.println("  "+JetLatencyMonitor.class.getSimpleName()+" <bootstrap.servers> <topic> <slideByMs> <outputFile>");
             System.exit(1);
         }
         System.setProperty("hazelcast.logging.type", "log4j");
         String brokerUri = args[0];
         String topic = args[1];
         int slideBy = Integer.parseInt(args[2]);
-        String fileName = args.length > 3 ? args[3] : null;
+        String outputFile = args[3];
 
         JetInstance jetInstance = Jet.newJetInstance();
 
         Properties kafkaProps = getKafkaProperties(brokerUri);
         WindowDefinition windowDef = WindowDefinition.slidingWindowDef(10000, slideBy);
-        WindowOperation<Trade, TupleLongLong, Long> windowOperation = WindowOperation.of(
+        WindowDefinition windowDefLatency = WindowDefinition.tumblingWindowDef(1000);
+        WindowOperation<Trade, ?, Long> winOpAverageTradePrice = averageLongToLong(Trade::getPrice);
+        WindowOperation<Frame<?, Long>, ?, Long> winOpAverageLatency = averageLongToLong(Frame::getValue);
+
+        int lag = 1000;
+
+        DAG dag = new DAG();
+        Vertex readKafka = dag.newVertex("readKafka", streamKafka(kafkaProps, topic));
+        Vertex extractTrade = dag.newVertex("extractTrade", map(entryValue()));
+        Vertex insertPunctuation = dag.newVertex("insertPunctuation",
+                insertPunctuation(Trade::getTime, () -> cappingEventSeqLag(lag)
+                        .throttleByFrame(windowDef)));
+        Vertex groupByF = dag.newVertex("groupByF",
+                groupByFrame(Trade::getTicker, Trade::getTime, windowDef, winOpAverageTradePrice));
+        Vertex slidingW = dag.newVertex("slidingW",
+                slidingWindow(windowDef, winOpAverageTradePrice));
+        Vertex mapToLatency = dag.newVertex("mapToLatency",
+                map((Frame frame) -> new Frame<>(frame.getSeq(), 0, System.currentTimeMillis() - frame.getSeq() - lag)));
+        Vertex groupByF2 = dag.newVertex("groupByF2",
+                groupByFrame(Frame::getKey, frame -> frame.getSeq() - 1, windowDefLatency, winOpAverageLatency));
+        Vertex slidingW2 = dag.newVertex("slidingW2",
+                slidingWindow(windowDefLatency, winOpAverageLatency));
+        Vertex fileSink = dag.newVertex("fileSink", writeFile(outputFile)).localParallelism(1);
+
+        dag
+                .edge(between(readKafka, extractTrade)
+                        .oneToMany())
+                .edge(between(extractTrade, insertPunctuation)
+                        .oneToMany())
+                .edge(between(insertPunctuation, groupByF)
+                        .partitioned(Trade::getTicker, HASH_CODE))
+                .edge(between(groupByF, slidingW)
+                        .partitioned(Frame<Object, Object>::getKey)
+                        .distributed())
+                .edge(between(slidingW, mapToLatency)
+                        .oneToMany())
+                .edge(between(mapToLatency, groupByF2)
+                        .oneToMany())
+                .edge(between(groupByF2, slidingW2)
+                        .partitioned((Function<Frame, Object>) Frame::getKey)
+                        .distributed())
+                .edge(between(slidingW2, fileSink));
+
+        // peeks
+        Vertex peek = dag.newVertex("peek", PeekP::new);
+        dag.edge(from(slidingW2, 1).to(peek).oneToMany());
+//        dag.edge(from(slidingW, 1).to(peek));
+
+        ClientConfig clientConfig = new ClientConfig();
+        clientConfig.getGroupConfig().setName("jet");
+        clientConfig.getGroupConfig().setPassword("jet-pass");
+        JetInstance client = Jet.newJetClient(clientConfig);
+        client.newJob(dag).execute();
+    }
+
+    private static <T> WindowOperation<T, ?, Long> averageLongToLong(Distributed.ToLongFunction<T> mapper) {
+        return WindowOperation.of(
                 TupleLongLong::new,
-                (acc, trade) -> {
-                    acc.sum = Math.addExact(acc.sum, trade.getPrice());
+                (acc, item) -> {
+                    acc.sum = Math.addExact(acc.sum, mapper.applyAsLong(item));
                     acc.count++;
                 },
                 (acc1, acc2) -> {
@@ -70,103 +118,23 @@ public class JetLatencyMonitor {
                     acc1.count = Math.subtractExact(acc1.count, acc2.count);
                     return acc1;
                 },
-                acc -> acc.sum / acc.count
+                acc -> acc.count > 0 ? acc.sum / acc.count : Long.MIN_VALUE
         );
-
-        int lag = 1000;
-
-        DAG dag = new DAG();
-        Vertex readKafka = dag.newVertex("readKafka", streamKafka(kafkaProps, topic));
-        Vertex extractTrade = dag.newVertex("extractTrade", map(entryValue()));
-        Vertex insertPunctuation = dag.newVertex("insertPunctuation",
-                insertPunctuation(Trade::getTime, () -> cappingEventSeqLag(lag)
-                        .throttleByFrame(windowDef)));
-        Vertex groupByF = dag.newVertex("groupByF",
-                WindowingProcessors.groupByFrame(Trade::getTicker, Trade::getTime, windowDef, windowOperation));
-        Vertex slidingW = dag.newVertex("slidingW",
-                slidingWindow(windowDef, windowOperation));
-        Vertex sink1 = dag.newVertex("sink1",
-                () -> new Processor() {
-                    public ILogger logger;
-
-                    @Override
-                    public void init(Outbox outbox, Context context) {
-                        logger = context.logger();
-                    }
-
-                    @Override
-                    public void process(int ordinal, Inbox inbox) {
-                        long now = System.currentTimeMillis();
-                        long localSum = 0, localCount = 0;
-                        for (Object o; (o = inbox.poll()) != null; ) {
-                            if (o instanceof Punctuation) {
-                                continue;
-                            }
-                            Frame<String, Long> frame = (Frame<String, Long>) o;
-                            long latency = now - frame.getSeq();
-                            localSum += latency;
-                            localCount++;
-                        }
-
-                        totalSum.addAndGet(localSum);
-                        totalCount.addAndGet(localCount);
-                    }
-                }).localParallelism(1);
-
-        dag
-                .edge(between(readKafka, extractTrade).oneToMany())
-                .edge(between(extractTrade, insertPunctuation).oneToMany())
-                .edge(between(insertPunctuation, groupByF).partitioned(Trade::getTicker, HASH_CODE))
-                .edge(between(groupByF, slidingW).partitioned(Frame<Object, Object>::getKey)
-                                                 .distributed())
-                .edge(between(slidingW, sink1));
-
-        // add the file sink, if requested
-        if (fileName != null) {
-            Vertex sink2 = dag.newVertex("sink2", writeFile(fileName)).localParallelism(1);
-            dag.edge(from(slidingW, 1).to(sink2));
-        }
-
-        ClientConfig clientConfig = new ClientConfig();
-        clientConfig.getGroupConfig().setName("jet");
-        clientConfig.getGroupConfig().setPassword("jet-pass");
-        JetInstance client = Jet.newJetClient(clientConfig);
-        client.newJob(dag).execute();
-
-        while (true) {
-            Thread.sleep(1000);
-            long sum = totalSum.get();
-            long count = totalCount.get();
-            totalSum.set(0);
-            totalCount.set(0);
-            System.out.println("average latency=" + (count != 0 ? (sum / count - lag) + "ms" : "?") + ", count=" + count);
-        }
     }
 
     static class PeekP extends AbstractProcessor {
 
-        private long start;
-
-        @Override
-        protected void init(Context context) throws Exception {
-            start = System.currentTimeMillis();
-            super.init(context);
-        }
-
         @Override
         protected boolean tryProcess(int ordinal, Object item) throws Exception {
-            Map.Entry e = (Map.Entry) item;
-            if (e.getKey().equals("AAPL")) {
-                getLogger().info("Window: " + item);
-                getLogger().info("Elapsed: " + (System.currentTimeMillis() - start));
-            }
+            System.out.println(item);
             return true;
         }
 
-        @Override
-        protected boolean tryProcessPunc(int ordinal, Punctuation punc) {
-            return true;
-        }
+//        @Override
+//        protected boolean tryProcessPunc(int ordinal, Punctuation punc) {
+//            System.out.println(punc);
+//            return true;
+//        }
     }
 
     private static Properties getKafkaProperties(String brokerUrl) {
