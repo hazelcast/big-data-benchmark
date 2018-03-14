92668882f0d2e70bf9c62ca65669eb5121ca3f1a
Viliam Durina <viliam-durina@users.noreply.github.com>
2018-03-14 12:03:07 +0100

    Add choice of source/sink parallelism, message type

--- trade-monitor/jet-trade-monitor/src/main/java/com/hazelcast/jet/benchmark/trademonitor/JetTradeMonitor.java
+++ trade-monitor/jet-trade-monitor/src/main/java/com/hazelcast/jet/benchmark/trademonitor/JetTradeMonitor.java
@@ -2,60 +2,81 @@ package com.hazelcast.jet.benchmark.trademonitor;
 
 import com.hazelcast.jet.JetInstance;
 import com.hazelcast.jet.Job;
+import com.hazelcast.jet.benchmark.trademonitor.RealTimeTradeProducer.MessageType;
 import com.hazelcast.jet.config.JobConfig;
 import com.hazelcast.jet.config.ProcessingGuarantee;
 import com.hazelcast.jet.kafka.KafkaSources;
+import com.hazelcast.jet.pipeline.ContextFactory;
 import com.hazelcast.jet.pipeline.Pipeline;
 import com.hazelcast.jet.pipeline.Sinks;
 import com.hazelcast.jet.pipeline.StreamStage;
 import com.hazelcast.jet.server.JetBootstrap;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.ByteArrayDeserializer;
 import org.apache.kafka.common.serialization.LongDeserializer;
 
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
 import java.time.Instant;
 import java.time.ZoneId;
+import java.util.Arrays;
 import java.util.Properties;
 import java.util.UUID;
 
 import static com.hazelcast.jet.aggregate.AggregateOperations.counting;
+import static com.hazelcast.jet.benchmark.trademonitor.RealTimeTradeProducer.MessageType.BYTE;
 import static com.hazelcast.jet.pipeline.WindowDefinition.sliding;
 import static java.lang.System.currentTimeMillis;
 
 public class JetTradeMonitor {
 
     public static void main(String[] args) throws Exception {
-        if (args.length != 10) {
+        System.out.println("Arguments: " + Arrays.toString(args));
+        if (args.length != 13) {
             System.err.println("Usage:");
             System.err.println("  " + JetTradeMonitor.class.getSimpleName() +
                     " <bootstrap.servers> <topic> <offset-reset> <maxLagMs> <windowSizeMs> <slideByMs>" +
-                    " <snapshotIntervalMs> <processingGuarantee> <outputPath> <latencyType>");
+                    " <snapshotIntervalMs> <processingGuarantee> <outputPath> <latencyType> <kafkaParallelism>" +
+                    " <sinkParallelism> <messageType>");
             System.err.println();
             System.err.println("<processingGuarantee> - \"none\" or \"exactly-once\" or \"at-least-once\"");
             System.err.println("<latencyType> - \"internal-latency\" or \"external-latency\"");
+            System.err.println("<messageType> - byte|object");
             System.exit(1);
         }
         System.setProperty("hazelcast.logging.type", "log4j");
         String brokerUri = args[0];
         String topic = args[1];
         String offsetReset = args[2];
-        int lagMs = Integer.parseInt(args[3]);
-        int windowSize = Integer.parseInt(args[4]);
-        int slideBy = Integer.parseInt(args[5]);
-        int snapshotInterval = Integer.parseInt(args[6]);
+        int lagMs = Integer.parseInt(args[3].replace("_", ""));
+        int windowSize = Integer.parseInt(args[4].replace("_", ""));
+        int slideBy = Integer.parseInt(args[5].replace("_", ""));
+        int snapshotInterval = Integer.parseInt(args[6].replace("_", ""));
         ProcessingGuarantee guarantee = ProcessingGuarantee.valueOf(args[7].toUpperCase().replace('-', '_'));
         String outputPath = args[8];
         LatencyType latencyType = LatencyType.valueOf(args[9].toUpperCase().replace('-', '_'));
+        int kafkaParallelism = Integer.parseInt(args[10]);
+        int sinkParallelism = Integer.parseInt(args[11]);
+        MessageType messageType = MessageType.valueOf(args[12].toUpperCase());
 
-        Properties kafkaProps = getKafkaProperties(brokerUri, offsetReset);
+        Properties kafkaProps = getKafkaProperties(brokerUri, offsetReset, messageType);
 
         Pipeline p = Pipeline.create();
-        StreamStage<Trade> sourceStage = p.drawFrom(KafkaSources.kafka(kafkaProps, (ConsumerRecord<Object, Trade> record) -> record.value(), topic))
-                .setLocalParallelism(24);
+        StreamStage<Trade> sourceStage;
+        if (messageType == BYTE) {
+            sourceStage = p.drawFrom(KafkaSources.kafka(kafkaProps, (ConsumerRecord<Object, byte[]> record) -> record.value(), topic))
+                                              .setLocalParallelism(kafkaParallelism)
+                                              .mapUsingContext(ContextFactory.withCreateFn(jet -> new DeserializeContext()), JetTradeMonitor::deserialize);
+        } else {
+            sourceStage = p.drawFrom(KafkaSources.kafka(kafkaProps, (ConsumerRecord<Object, Trade> record) -> record.value(), topic))
+                                              .setLocalParallelism(kafkaParallelism);
+        }
         if (latencyType == LatencyType.INTERNAL_LATENCY) {
             sourceStage = sourceStage.addTimestamps();
         } else {
             sourceStage = sourceStage.addTimestamps(Trade::getTime, lagMs);
         }
+        sourceStage = sourceStage.setLocalParallelism(kafkaParallelism);
 
         sourceStage
                 .groupingKey(Trade::getTicker)
@@ -72,7 +93,8 @@ public class JetTradeMonitor {
                             + "," + timeMs
                             + "," + (latencyMs);
                 })
-                .drainTo(Sinks.files(outputPath));
+                .drainTo(Sinks.files(outputPath))
+                .setLocalParallelism(sinkParallelism);
 
         // uncomment one of the following lines
 //        JetInstance jet = Jet.newJetInstance(); // uncomment for local execution
@@ -80,6 +102,7 @@ public class JetTradeMonitor {
 
         System.out.println("Executing job..");
         JobConfig config = new JobConfig();
+        config.addClass(JetTradeMonitor.class);
         config.setSnapshotIntervalMillis(snapshotInterval);
         config.setProcessingGuarantee(guarantee);
         Job job = jet.newJob(p, config);
@@ -92,12 +115,25 @@ public class JetTradeMonitor {
         jet.shutdown();
     }
 
-    private static Properties getKafkaProperties(String brokerUrl, String offsetReset) {
+    private static Trade deserialize(DeserializeContext ctx, byte[] bytes) {
+        ctx.is.setBuffer(bytes);
+        try {
+            String ticker = ctx.ois.readUTF();
+            long time = ctx.ois.readLong();
+            int price = ctx.ois.readInt();
+            int quantity = ctx.ois.readInt();
+            return new Trade(time, ticker, quantity, price);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Properties getKafkaProperties(String brokerUrl, String offsetReset, MessageType messageType) {
         Properties props = new Properties();
         props.setProperty("bootstrap.servers", brokerUrl);
         props.setProperty("group.id", UUID.randomUUID().toString());
         props.setProperty("key.deserializer", LongDeserializer.class.getName());
-        props.setProperty("value.deserializer", TradeDeserializer.class.getName());
+        props.setProperty("value.deserializer", (messageType == BYTE ? ByteArrayDeserializer.class : TradeDeserializer.class).getName());
         props.setProperty("auto.offset.reset", offsetReset);
         props.setProperty("max.poll.records", "32768");
         //props.setProperty("metadata.max.age.ms", "5000");
@@ -107,5 +143,24 @@ public class JetTradeMonitor {
     private enum LatencyType {
         INTERNAL_LATENCY,
         EXTERNAL_LATENCY
+    }
+
+    private static class DeserializeContext {
+        final ResettableByteArrayInputStream is = new ResettableByteArrayInputStream();
+        final DataInputStream ois = new DataInputStream(is);
+    }
+
+    private static class ResettableByteArrayInputStream extends ByteArrayInputStream {
+
+        ResettableByteArrayInputStream() {
+            super(new byte[0]);
+        }
+
+        void setBuffer(byte[] data) {
+            super.buf = data;
+            super.pos = 0;
+            super.mark = 0;
+            super.count = data.length;
+        }
     }
 }
--- trade-monitor/kafka-trade-producer/src/main/java/com.hazelcast.jet.benchmark.trademonitor/RealTimeTradeProducer.java
+++ trade-monitor/kafka-trade-producer/src/main/java/com.hazelcast.jet.benchmark.trademonitor/RealTimeTradeProducer.java
@@ -2,8 +2,12 @@ package com.hazelcast.jet.benchmark.trademonitor;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.ByteArraySerializer;
 import org.apache.kafka.common.serialization.LongSerializer;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Properties;
 import java.util.concurrent.ExecutorService;
@@ -17,40 +21,51 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 public class RealTimeTradeProducer implements Runnable {
 
     private static final int BATCH_SIZE = 100_000;
-    private final KafkaProducer<Long, Trade> producer;
+    private final KafkaProducer<Long, Object> producer;
     private final int index;
     private final String topic;
     private final int tradesPerSecond;
+    private final MessageType messageType;
     private String[] tickers;
     private int tickerIndex;
 
-    private RealTimeTradeProducer(int index, String broker, String topic, int tradesPerSecond, int keysFrom, int keysTo) {
+    private RealTimeTradeProducer(int index, String broker, String topic, int tradesPerSecond, int keysFrom, int keysTo, MessageType messageType) {
         if (tradesPerSecond <= 0) {
             throw new RuntimeException("tradesPerSecond=" + tradesPerSecond);
         }
         this.index = index;
         this.topic = topic;
         this.tradesPerSecond = tradesPerSecond;
+        this.messageType = messageType;
+
         tickers = new String[keysTo - keysFrom];
         Arrays.setAll(tickers, i -> "T-" + Integer.toString(i + keysFrom));
         Properties props = new Properties();
         props.setProperty("bootstrap.servers", broker);
         props.setProperty("key.serializer", LongSerializer.class.getName());
-        props.setProperty("value.serializer", TradeSerializer.class.getName());
+        if (messageType == MessageType.BYTE) {
+            props.setProperty("value.serializer", ByteArraySerializer.class.getName());
+        } else {
+            props.setProperty("value.serializer", TradeSerializer.class.getName());
+        }
         this.producer = new KafkaProducer<>(props);
     }
 
     public static void main(String[] args) {
-        if (args.length != 5) {
+        if (args.length != 6) {
             System.err.println("Usage:");
-            System.err.println("  " + RealTimeTradeProducer.class.getSimpleName() + " <bootstrap.servers> <topic> <num producers> <trades per second> <num distinct keys>");
+            System.err.println("  " + RealTimeTradeProducer.class.getSimpleName()
+                    + " <bootstrap.servers> <topic> <num producers> <trades per second> <num distinct keys> <messageType>");
+            System.err.println();
+            System.err.println("<messageType> - byte|object");
             System.exit(1);
         }
         String broker = args[0];
         String topic = args[1];
-        int numProducers = Integer.parseInt(args[2]);
-        int tradesPerSecond = Integer.parseInt(args[3]);
-        int numDistinctKeys = Integer.parseInt(args[4]);
+        int numProducers = Integer.parseInt(args[2].replace("_", ""));
+        int tradesPerSecond = Integer.parseInt(args[3].replace("_", ""));
+        int numDistinctKeys = Integer.parseInt(args[4].replace("_", ""));
+        MessageType messageType = MessageType.valueOf(args[5].toUpperCase());
 
         ExecutorService executorService = Executors.newFixedThreadPool(numProducers);
         int keysPerProducer = numDistinctKeys / numProducers;
@@ -65,21 +80,44 @@ public class RealTimeTradeProducer implements Runnable {
         }
         for (int i = 0; i < numProducers; i++) {
             RealTimeTradeProducer tradeProducer = new RealTimeTradeProducer(i, broker, topic, tradesPerSecondPerProducer,
-                    keysPerProducer * i, keysPerProducer * (i + 1));
+                    keysPerProducer * i, keysPerProducer * (i + 1), messageType);
             executorService.submit(tradeProducer);
         }
     }
 
-    private void send(String topic, Trade trade) {
-        producer.send(new ProducerRecord<>(topic, null, null, trade));
+    private void send(ByteArrayOutputStream baos, DataOutputStream oos, String topic, Trade trade, MessageType messageType) {
+        Object msgObject;
+        if (messageType == MessageType.BYTE) {
+            msgObject = serialize(baos, oos, trade);
+        } else {
+            msgObject = trade;
+        }
+        producer.send(new ProducerRecord<>(topic, msgObject));
+    }
+
+    private byte[] serialize(ByteArrayOutputStream baos, DataOutputStream oos, Trade trade) {
+        try {
+            oos.writeUTF(trade.getTicker());
+            oos.writeLong(trade.getTime());
+            oos.writeInt(trade.getPrice());
+            oos.writeInt(trade.getQuantity());
+            return baos.toByteArray();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            baos.reset();
+        }
     }
 
     private void runNonThrottled() {
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        final DataOutputStream oos = new DataOutputStream(baos);
+
         long start = System.nanoTime();
         long produced = 0;
         while (true) {
             for (int i = 0; i < BATCH_SIZE; i++) {
-                send(topic, nextTrade(System.currentTimeMillis()));
+                send(baos, oos, topic, nextTrade(System.currentTimeMillis()), messageType);
             }
             produced += BATCH_SIZE;
 
@@ -100,11 +138,14 @@ public class RealTimeTradeProducer implements Runnable {
         int lastSecond = 0;
         int producedSinceLastPrinted = 0;
 
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        final DataOutputStream oos = new DataOutputStream(baos);
+
         while (true) {
             long now = System.nanoTime();
             final long expectedProduced = NANOSECONDS.toSeconds((now - start) * tradesPerSecond);
             for (int i = 0; totalTradesProduced < expectedProduced && i < BATCH_SIZE; i++) {
-                send(topic, nextTrade(System.currentTimeMillis()));
+                send(baos, oos, topic, nextTrade(System.currentTimeMillis()), messageType);
                 producedSinceLastPrinted++;
                 totalTradesProduced++;
             }
@@ -134,5 +175,10 @@ public class RealTimeTradeProducer implements Runnable {
 //            lag = 0;
 //        }
         return new Trade(time, ticker, 100, 10000);
+    }
+
+    public enum MessageType {
+        BYTE,
+        OBJECT
     }
 }
