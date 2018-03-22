b61372323f96b2be1a210f72ab577c54df8fd8cb
Can Gencer <cgencer@gmail.com>
2018-03-22 11:22:42 +0100

    Add parser script

---
+++ trade-monitor/jet-trade-monitor/src/main/resources/parse_jet.py
@@ -0,0 +1,16 @@
+import fileinput
+import sys
+
+total=0
+count=0
+min_delay=float('inf')
+max_delay=0
+
+for line in fileinput.input():
+    delay=int(line.strip('()\n').split(',')[4]) - 1000
+    min_delay=min(min_delay, delay)
+    max_delay=max(max_delay, delay)
+    total+=delay
+    count+=1
+
+print "avg=%d, min=%d, max=%d" % (total/count, min_delay, max_delay)
