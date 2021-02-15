3a84ddf51b94ae1d32b81498d001ab017a6399c8
Viliam Durina <viliam-durina@users.noreply.github.com>
2021-02-15 20:41:49 +0100

    WIP

--- nexmark-jet/src/main/java/com/hazelcast/jet/benchmark/nexmark/model/Auction.java
+++ nexmark-jet/src/main/java/com/hazelcast/jet/benchmark/nexmark/model/Auction.java
@@ -3,12 +3,19 @@ package com.hazelcast.jet.benchmark.nexmark.model;
 public class Auction extends Event {
     private final long sellerId;
 
-    public Auction(long id, long timestamp, long sellerId) {
+    private final int category;
+
+    public Auction(long id, long timestamp, long sellerId, int category) {
         super(id, timestamp);
         this.sellerId = sellerId;
+        this.category = category;
     }
 
     public long sellerId() {
         return sellerId;
+    }
+
+    public int category() {
+        return category;
     }
 }
