59fb71c75840e22bb28132a647006cae78739e5e
Marko Topolnik <marko@hazelcast.com>
2021-02-16 19:56:23 +0100

    Productionize Query 3

--- nexmark-jet/src/main/java/com/hazelcast/jet/benchmark/nexmark/Q03LocalItemSuggestion.java
+++ nexmark-jet/src/main/java/com/hazelcast/jet/benchmark/nexmark/Q03LocalItemSuggestion.java
@@ -1,6 +1,7 @@
 package com.hazelcast.jet.benchmark.nexmark;
 
 import com.hazelcast.jet.Traverser;
+import com.hazelcast.jet.Traversers;
 import com.hazelcast.jet.benchmark.nexmark.model.Auction;
 import com.hazelcast.jet.benchmark.nexmark.model.Person;
 import com.hazelcast.jet.datamodel.Tuple2;
@@ -13,8 +14,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+import static com.hazelcast.jet.Traversers.empty;
 import static com.hazelcast.jet.Traversers.singleton;
 import static com.hazelcast.jet.Traversers.traverseIterable;
+import static com.hazelcast.jet.benchmark.nexmark.EventSourceP.eventSource;
 import static com.hazelcast.jet.datamodel.Tuple5.tuple5;
 
 public class Q03LocalItemSuggestion extends BenchmarkBase {
@@ -29,35 +32,46 @@ public class Q03LocalItemSuggestion extends BenchmarkBase {
     StreamStage<Tuple2<Long, Long>> addComputation(
             Pipeline pipeline, Properties props
     ) throws ValidationException {
+        // This code respects numDistinctKeys indirectly, by creating a pattern of
+        // seller IDs over time so that there are auctions with a given seller ID
+        // for a limited time. A "cloud" of random seller IDs slowly moves up the
+        // integer line. It advances by one every auctionsPerPersonEvent. The width
+        // of the cloud is numDistinckKeys. We calculate the TTL for the keyed
+        // mapStateful stage to match the amount of time during which this cloud
+        // covers any given seller ID.
         int numDistinctKeys = parseIntProp(props, PROP_NUM_DISTINCT_KEYS);
-        int auctionIdModulus = 128;
         int eventsPerSecond = parseIntProp(props, PROP_EVENTS_PER_SECOND);
-        int sievingFactor = Math.max(1, eventsPerSecond / (8192 * auctionIdModulus));
+        int auctionsPerPersonEvent = 100;
+        long ttl = (long) numDistinctKeys * auctionsPerPersonEvent * 1000 / eventsPerSecond;
 
         StreamStage<Object> persons = pipeline
-                .readFrom(EventSourceP.eventSource(eventsPerSecond, INITIAL_SOURCE_DELAY_MILLIS, (seq, timestamp) -> {
-                    long id = getRandom(seq, numDistinctKeys);
-                    return new Person(id, timestamp, "Seller #" + id, STATES[seq.intValue() % STATES.length]);
-                }))
+                .readFrom(eventSource(eventsPerSecond / auctionsPerPersonEvent, INITIAL_SOURCE_DELAY_MILLIS,
+                        (seq, timestamp) -> {
+                            long id = seq;
+                            return new Person(id, timestamp, "Seller #" + id, STATES[seq.intValue() % STATES.length]);
+                        }))
                 .withNativeTimestamps(0)
                 .filter(p  -> p.state().equals("OR") || p.state().equals("CA") || p.state().equals("ID"))
-                .map(p -> p);
+                .map(p -> p); // upcast
 
         StreamStage<Object> auctions = pipeline
-                .readFrom(EventSourceP.eventSource(eventsPerSecond, INITIAL_SOURCE_DELAY_MILLIS, (seq, timestamp) -> {
-                    long sellerId = getRandom(137 * seq, numDistinctKeys);
-                    return new Auction(seq, timestamp, sellerId, 0, 0);
+                .readFrom(eventSource(eventsPerSecond, INITIAL_SOURCE_DELAY_MILLIS, (seq, timestamp) -> {
+                    long sellerId = seq / auctionsPerPersonEvent - getRandom(seq, numDistinctKeys);
+                    if (sellerId < 0) {
+                        return new Auction(seq, timestamp, 0, 1); // will be filtered out
+                    }
+                    int categoryId = (int) getRandom(seq, 10);
+                    return new Auction(seq, timestamp, sellerId, categoryId);
                 }))
                 .withNativeTimestamps(0)
-                .filter(a -> a.category() == 10)
-                .map(p -> p);
+                .filter(a -> a.category() == 0)
+                .map(p -> p); // upcast
 
         return persons
                 .merge(auctions)
                 .groupingKey(o -> o instanceof Person ? ((Person) o).id() : ((Auction) o).sellerId())
-                .flatMapStateful(SellerAuctionJoin::new, SellerAuctionJoin::flatMap)
+                .flatMapStateful(ttl, SellerAuctionJoin::new, SellerAuctionJoin::flatMap, null)
 
-                .filter(t4 -> t4.f3() % sievingFactor == 0)
                 .apply(stage -> determineLatency(stage, Tuple5::f2));
     }
 
@@ -75,8 +89,10 @@ public class Q03LocalItemSuggestion extends BenchmarkBase {
             } else {
                 Auction auction = (Auction) o;
                 auctions.add(auction);
-                return singleton(tuple5(person.name(), person.state(), auction.timestamp(), auction.id(),
-                        auction.category()));
+                return person != null
+                        ? singleton(tuple5(person.name(), person.state(), auction.timestamp(), auction.id(),
+                        auction.category()))
+                        : empty();
             }
         }
     }
