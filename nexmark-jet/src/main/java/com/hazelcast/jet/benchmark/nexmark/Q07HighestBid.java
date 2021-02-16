224b3f35a220a83b2038db97f3b23360a1a2735c
Viliam Durina <viliam-durina@users.noreply.github.com>
2021-02-16 18:40:22 +0100

    Add nexmark query 7

---
+++ nexmark-jet/src/main/java/com/hazelcast/jet/benchmark/nexmark/Q07HighestBid.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2008-2019, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.jet.benchmark.nexmark;
+
+import com.hazelcast.jet.benchmark.nexmark.model.Bid;
+import com.hazelcast.jet.datamodel.Tuple2;
+import com.hazelcast.jet.datamodel.Tuple3;
+import com.hazelcast.jet.pipeline.Pipeline;
+import com.hazelcast.jet.pipeline.StreamStage;
+
+import java.util.Properties;
+
+import static com.hazelcast.function.ComparatorEx.comparing;
+import static com.hazelcast.jet.aggregate.AggregateOperations.maxBy;
+import static com.hazelcast.jet.pipeline.WindowDefinition.tumbling;
+import static java.util.concurrent.TimeUnit.MINUTES;
+
+public class Q07HighestBid extends BenchmarkBase {
+
+    @Override
+    StreamStage<Tuple2<Long, Long>> addComputation(
+            Pipeline pipeline, Properties props
+    ) throws ValidationException {
+        int numDistinctKeys = parseIntProp(props, PROP_NUM_DISTINCT_KEYS);
+        int auctionIdModulus = 128;
+        int eventsPerSecond = parseIntProp(props, PROP_EVENTS_PER_SECOND);
+        int sievingFactor = Math.max(1, eventsPerSecond / (8192 * auctionIdModulus));
+        int auctionBidRatio = 10;
+        long auctionMaxDuration = 1024;
+
+        pipeline
+                .readFrom(EventSourceP.eventSource(eventsPerSecond, INITIAL_SOURCE_DELAY_MILLIS,
+                        (seq, timestamp) -> new Bid(seq, timestamp, seq, 0)))
+                .withNativeTimestamps(0)
+                .window(tumbling(MINUTES.toMillis(1)))
+                .aggregate(maxBy(comparing(Bid::price)))
+
+                .filter(bid -> bid.result().timestamp() % sievingFactor == 0)
+                .apply(stage -> determineLatency(stage, res -> res.result().timestamp()));
+    }
+}
